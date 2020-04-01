06ff06ba1200e44ae66417b21aa51027503ef1fa
Forrest Fuqua <kingjrwr@gmail.com>
2020-04-01 19:09:35 -0400

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit April Fools Imposter Bot
 // @namespace    jrwr.io
-// @version      1.0.2
+// @version      1.0.3
 // @description  A bot that randomly chooses a entry and reports back to a central database at spacescience.tech
 // @author       dimden updated by jrwr
 // @match        https://gremlins-api.reddit.com/room?nightmode=1&platform=desktop
