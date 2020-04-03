d7f709e8355174c4a2990044086b26a4358ee320
dimden <admin@dimden.dev>
2020-04-03 15:21:28 +0300

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reddit April Fools Imposter Bot
 // @namespace    jrwr.io
-// @version      1.1.8
+// @version      1.1.8.1
 // @description  A bot that randomly chooses a entry and reports back to a central database at spacescience.tech
 // @author       dimden (https://dimden.dev/), jrwr (http://jrwr.io/), px(u/Hennihenner), qqii, cg
 // @match        https://gremlins-api.reddit.com/room?nightmode=1&platform=desktop
